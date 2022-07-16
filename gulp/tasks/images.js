2865e8a254aa5c7ddd30346476499981e88bc217
Sergey <melnykserhiy1@gmail.com>
2022-07-16 23:42:37 +0300

    initial new gulp v4

---
+++ gulp/tasks/images.js
@@ -0,0 +1,42 @@
+var gulp 		= require('gulp');
+var config 		= require('../config');
+var size 		= require('gulp-size');
+var newer 		= require('gulp-newer');
+var mode 		= require('gulp-mode')();
+var imagemin 	= require('gulp-imagemin');
+
+const imageminOptions = {
+	mozjpeg: {
+		quality: 75,
+		progressive: true,
+	},
+	optipng: {
+		optimizationLevel: 5,
+	},
+	svgo: {
+		plugins: [
+			{ removeViewBox: false },
+			{ cleanupIDs: false },
+			{ removeComments: true },
+			{ removeEmptyContainers: true },
+		],
+	},
+};
+
+gulp.task('images', (done) => {
+	gulp.src(config.src.img+'*.*')
+	// .pipe(newer(config.src.img+'*/*'))
+	.pipe(
+		mode.production(
+			imagemin([
+				imagemin.mozjpeg(imageminOptions.mozjpeg),
+				imagemin.optipng(imageminOptions.optipng),
+				imagemin.svgo(imageminOptions.svgo),
+			]),
+		),
+	)
+	.pipe(size({ showFiles: true }))
+	.pipe(gulp.dest(config.dest.img));
+
+	done()
+})
